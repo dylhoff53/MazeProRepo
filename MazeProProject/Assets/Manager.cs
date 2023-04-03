1ffa0e0fd818b6c0d2aa3f7aa93e9833aba39f01
dhoffman548 <112724937+dylhoff53@users.noreply.github.com>
2023-04-03 01:25:09 -0700

    th

--- MazeProProject/Assets/Manager.cs
+++ MazeProProject/Assets/Manager.cs
@@ -7,17 +7,37 @@ public class Manager : MonoBehaviour
     public GameObject mazeMaker;
     public MazeMaker secondMaze;
     public MazeMaker firstMaze;
+    public GameObject thirdMazeMaker;
+    public MazeMaker thirdMaze;
 
     public void Update()
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
             MakeNewMaze();
+        }else if (Input.GetKeyDown(KeyCode.R))
+        {
+            MakeThirdMaze();
+        } else if (Input.GetKeyDown(KeyCode.T))
+        {
+            MakeSecondMaze();
         }
     }
     public void MakeNewMaze()
     {
         mazeMaker.transform.position = new Vector3(0, 0, firstMaze.mazeHeight);
+        secondMaze.MakeMaze();
+    }
+
+    public void MakeThirdMaze()
+    {
+        thirdMazeMaker.transform.position = new Vector3(0, 0, secondMaze.mazeHeight);
+        thirdMaze.MakeMaze();
+    }
+    public void MakeSecondMaze()
+    {
+        mazeMaker.transform.position = new Vector3(0, 0, thirdMaze.mazeHeight);
+        secondMaze.otherMazeMaker = thirdMaze;
         secondMaze.MakeMaze();
     }
 }
